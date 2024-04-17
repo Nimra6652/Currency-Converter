7470aac79b89be4a221d51964e7233c0323778b6
Nimra <161188938+Nimra6652@users.noreply.github.com>
2024-04-17 22:26:28 +0500

    currency conversions

---
+++ main.ts
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.yellow.bold("\n Welcome To 'CodeWithNimra' - Currency Converter \n"))
+
+
+let exchange_rate: any = {
+  "USD": 1,
+  "EUR": 0.9,
+  "JYP": 113,
+  "CAD": 1.3,
+  "AUD": 1.65,
+  "PKR": 277.70,
+}
+
+let user_answer = await inquirer.prompt([
+  {
+    name: "from_currency",
+    type: "list",
+    message: chalk.blue("\n Select the currency to convert from \n"),
+    choices:["USD", "EUR","JYP","CAD","AUD","PKR"]
+  },
+  {
+    name: "to_currency",
+    type: "list",
+    message:chalk.blue("\nSelect the currency to convert into:\n"),
+    choices:["USD", "EUR","JYP","CAD","AUD","PKR"]
+  },
+  {
+    name: "amount",
+    type: "input",
+    message: chalk.blue("\n Enter the amount to convert\n")
+  }
+]);
+//perform currency conversion by using formula
+let from_amount =  exchange_rate[user_answer.from_currency];
+let to_amount =  exchange_rate[user_answer.to_currency];
+let amount = user_answer.amount
+
+//formula of conversion
+let base_amount = amount / from_amount
+let converted_amount = base_amount * to_amount
+
+console.log (chalk.green(`\nconverted Amount= ${converted_amount}\n`));
